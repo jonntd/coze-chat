717f68cdb66fcd71ffbcb2822bdee343acb0aada
jonn <jonntd@gmail.com>
2024-06-08 01:06:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: "/coze-chat/",
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
